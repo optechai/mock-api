f2d92a522e132007a01d0e72c9eb22c5a01dba47
Xharmagne <xharmagne@gmail.com>
2024-02-22 15:58:09 +1100

    profile endpoint with email

--- api/index.js
+++ api/index.js
@@ -34,36 +34,36 @@ app.post('/api/user', function (req, res) {
 })
 
 
-// app.get('/auth/profile', function (req, res) {
-//     console.log("")
-//     console.log("-------- GET /auth/profile --------")
-//     console.log("request headers", req.headers)
-//     const response = {
-//         "profile": {
-//             "verifiedAliases": [
-//                 {
-//                     "userId": "u:Ch5tN2djanNLSW4wQ0NUMkZWTUU2d01NOWdDTkxGdms"
-//                 },
-//                 {
-//                     "phoneNumber": "16025559397"
-//                 },
-//                 {
-//                     "email": "dev-null+eSoVPT3DgKLO+nv@step.com"
-//                 },
-//                 {
-//                     "username": "user34989546150538856709"
-//                 }
-//             ],
-//             "unverifiedAliases": [],
-//             "groups": [],
-//             "blocked": false,
-//             "suspended": false,
-//             "createdAtMs": "1707514571743"
-//         }
-//     }
-//     console.log("response body", util.inspect(response, false, null, true /* enable colors */))
-//     res.send(response)
-// })
+app.get('/auth/profile', function (req, res) {
+    console.log("")
+    console.log("-------- GET /auth/profile --------")
+    console.log("request headers", req.headers)
+    const response = {
+        "profile": {
+            "verifiedAliases": [
+                {
+                    "userId": "u:123"
+                },
+                {
+                    "phoneNumber": "16025559397"
+                },
+                {
+                    "email": "thisisme@mail.com"
+                },
+                {
+                    "username": "user123"
+                }
+            ],
+            "unverifiedAliases": [],
+            "groups": [],
+            "blocked": false,
+            "suspended": false,
+            "createdAtMs": "1707514571743"
+        }
+    }
+    console.log("response body", util.inspect(response, false, null, true /* enable colors */))
+    res.send(response)
+})
 
 // app.post('/auth/login', function (req, res) {
 //     console.log("-------- POST /auth/login --------")
