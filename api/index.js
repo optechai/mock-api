5de427811e2d5d9a24bab4f672fa5bcc5d436361
Peter Nguyen <peter.quang.nguyen@gmail.com>
2024-03-01 16:20:38 +1100

    add user tier api mock

--- api/index.js
+++ api/index.js
@@ -1,134 +1,184 @@
-const express = require('express')
-const app = express()
-const util = require('util')
-const accounts = require('./accounts')
-const cards1 = require('./cards-1')
-const cards2 = require('./cards-2')
-const cardReplaced = require('./card-replaced')
-const user = require('./user')
+const express = require("express");
+const app = express();
+const util = require("util");
+const accounts = require("./accounts");
+const cards1 = require("./cards-1");
+const cards2 = require("./cards-2");
+const cardReplaced = require("./card-replaced");
+const user = require("./user");
+const userTier = require("./tier");
 
 app.use(express.json()); // for parsing application/json
 
-app.get('/api/user', function (req, res) {
-    console.log("")
-    console.log("-------- GET /user --------")
-    console.log("request headers", req.headers)
-    const response = user
-    console.log("response body", util.inspect(response, false, null, true /* enable colors */))
-    res.send(response)
-})
+app.get("/api/user", function (req, res) {
+  console.log("");
+  console.log("-------- GET /user --------");
+  console.log("request headers", req.headers);
+  const response = user;
+  console.log(
+    "response body",
+    util.inspect(response, false, null, true /* enable colors */)
+  );
+  res.send(response);
+});
 
+app.post("/api/user/shipping-address", function (req, res) {
+  console.log(`-------- POST /user/shipping-address --------`);
+  console.log("request headers", req.headers);
+  console.log(
+    "request body",
+    util.inspect(req.body, false, null, true /* enable colors */)
+  );
+  const response = { success: true };
+  console.log(
+    "response body",
+    util.inspect(response, false, null, true /* enable colors */)
+  );
+  res.send(response);
+});
 
-app.post('/api/user/shipping-address', function (req, res) {
-    console.log(`-------- POST /user/shipping-address --------`)
-    console.log("request headers", req.headers)
-    console.log("request body", util.inspect(req.body, false, null, true /* enable colors */))
-    const response = { success: true }
-    console.log("response body", util.inspect(response, false, null, true /* enable colors */))
-    res.send(response)
-})
+app.get("/api/user/orders", function (req, res) {
+  console.log("");
+  console.log("-------- GET /user/orders --------");
+  console.log("request headers", req.headers);
+  const response = {
+    orders: [
+      {
+        id: "1",
+        status: "In transit",
+        estimatedDeliveryDays: "3 days",
+      },
+    ],
+  };
+  console.log(
+    "response body",
+    util.inspect(response, false, null, true /* enable colors */)
+  );
+  res.send(response);
+});
 
-app.get('/api/user/orders', function (req, res) {
-    console.log("")
-    console.log("-------- GET /user/orders --------")
-    console.log("request headers", req.headers)
-    const response = {
-        "orders": [
-            {
-                "id": "1",
-                "status": "In transit",
-                "estimatedDeliveryDays": "3 days",
-            }
-        ]
-    }
-    console.log("response body", util.inspect(response, false, null, true /* enable colors */))
-    res.send(response)
-})
+app.get("/api/user/appointments", function (req, res) {
+  console.log("");
+  console.log("-------- GET /user/appointments --------");
+  console.log("request headers", req.headers);
+  const dateOne = new Date();
+  dateOne.setDate(dateOne.getDate() + 1);
+  dateOne.setHours(9, 0, 0, 0);
+  const dateTwo = new Date();
+  dateTwo.setDate(dateTwo.getDate() + 2);
+  dateTwo.setHours(11, 0, 0, 0);
+  const response = {
+    availableSlots: [
+      {
+        id: "1",
+        date: dateOne,
+      },
+      {
+        id: "2",
+        date: dateTwo,
+      },
+    ],
+  };
+  console.log(
+    "response body",
+    util.inspect(response, false, null, true /* enable colors */)
+  );
+  res.send(response);
+});
 
-app.get('/api/user/appointments', function (req, res) {
-    console.log("")
-    console.log("-------- GET /user/appointments --------")
-    console.log("request headers", req.headers)
-    const dateOne = new Date()
-    dateOne.setDate(dateOne.getDate() + 1)
-    dateOne.setHours(9, 0, 0, 0)
-    const dateTwo = new Date()
-    dateTwo.setDate(dateTwo.getDate() + 2)
-    dateTwo.setHours(11, 0, 0, 0)
-    const response = {
-        "availableSlots": [
-            {
-                "id": "1",
-                "date": dateOne,
-            },
-            {
-                "id": "2",
-                "date": dateTwo,
-            },
-        ]
-    }
-    console.log("response body", util.inspect(response, false, null, true /* enable colors */))
-    res.send(response)
-})
+app.get("/api/tier/user-tier", function (req, res) {
+  console.log("");
+  console.log("-------- GET /user/appointments --------");
+  console.log("request headers", req.headers);
+  const response = userTier;
+  console.log(
+    "request body",
+    util.inspect(response, false, null, true /* enable colors */)
+  );
+  res.send(response);
+});
 
-app.post('/api/appointments', function (req, res) {
-    console.log("-------- POST /appointments --------")
-    console.log("request headers", req.headers)
-    console.log("request body", util.inspect(req.body, false, null, true /* enable colors */))
-    const response = { success: true }
-    console.log("response body", util.inspect(response, false, null, true /* enable colors */))
-    res.send(response)
-})
+app.post("/api/appointments", function (req, res) {
+  console.log("-------- POST /appointments --------");
+  console.log("request headers", req.headers);
+  console.log(
+    "request body",
+    util.inspect(req.body, false, null, true /* enable colors */)
+  );
+  const response = { success: true };
+  console.log(
+    "response body",
+    util.inspect(response, false, null, true /* enable colors */)
+  );
+  res.send(response);
+});
 
-app.post('/api/card/replace', function (req, res) {
-    console.log("-------- POST /card/replace --------")
-    console.log("request headers", req.headers)
-    console.log("request body", util.inspect(req.body, false, null, true /* enable colors */))
-    const response = { success: true }
-    console.log("response body", util.inspect(response, false, null, true /* enable colors */))
-    res.send(response)
-})
+app.post("/api/card/replace", function (req, res) {
+  console.log("-------- POST /card/replace --------");
+  console.log("request headers", req.headers);
+  console.log(
+    "request body",
+    util.inspect(req.body, false, null, true /* enable colors */)
+  );
+  const response = { success: true };
+  console.log(
+    "response body",
+    util.inspect(response, false, null, true /* enable colors */)
+  );
+  res.send(response);
+});
 
-app.post('/api/user', function (req, res) {
-    console.log("-------- POST /user --------")
-    console.log("request headers", req.headers)
-    console.log("request body", util.inspect(req.body, false, null, true /* enable colors */))
-    const response = { success: true, updatedShippingAddress: req.body.shippingAddress }
-    console.log("response body", util.inspect(response, false, null, true /* enable colors */))
-    res.send(response)
-})
+app.post("/api/user", function (req, res) {
+  console.log("-------- POST /user --------");
+  console.log("request headers", req.headers);
+  console.log(
+    "request body",
+    util.inspect(req.body, false, null, true /* enable colors */)
+  );
+  const response = {
+    success: true,
+    updatedShippingAddress: req.body.shippingAddress,
+  };
+  console.log(
+    "response body",
+    util.inspect(response, false, null, true /* enable colors */)
+  );
+  res.send(response);
+});
 
-
-app.get('/api/profile', function (req, res) {
-    console.log("")
-    console.log("-------- GET /profile --------")
-    console.log("request headers", req.headers)
-    const response = {
-        "profile": {
-            "verifiedAliases": [
-                {
-                    "userId": "u:123"
-                },
-                {
-                    "phoneNumber": "16025559397"
-                },
-                {
-                    "email": "thisisme@mail.com"
-                },
-                {
-                    "username": "user123"
-                }
-            ],
-            "unverifiedAliases": [],
-            "groups": [],
-            "blocked": false,
-            "suspended": false,
-            "createdAtMs": "1707514571743"
-        }
-    }
-    console.log("response body", util.inspect(response, false, null, true /* enable colors */))
-    res.send(response)
-})
+app.get("/api/profile", function (req, res) {
+  console.log("");
+  console.log("-------- GET /profile --------");
+  console.log("request headers", req.headers);
+  const response = {
+    profile: {
+      verifiedAliases: [
+        {
+          userId: "u:123",
+        },
+        {
+          phoneNumber: "16025559397",
+        },
+        {
+          email: "thisisme@mail.com",
+        },
+        {
+          username: "user123",
+        },
+      ],
+      unverifiedAliases: [],
+      groups: [],
+      blocked: false,
+      suspended: false,
+      createdAtMs: "1707514571743",
+    },
+  };
+  console.log(
+    "response body",
+    util.inspect(response, false, null, true /* enable colors */)
+  );
+  res.send(response);
+});
 
 // app.post('/auth/login', function (req, res) {
 //     console.log("-------- POST /auth/login --------")
@@ -139,38 +189,46 @@ app.get('/api/profile', function (req, res) {
 //     res.send(response)
 // })
 
+app.get("/api/account", function (req, res) {
+  console.log("");
+  console.log("-------- GET /account --------");
+  console.log("request headers", req.headers);
+  const response = accounts;
+  console.log(
+    "response body",
+    util.inspect(response, false, null, true /* enable colors */)
+  );
+  res.send(response);
+});
 
-app.get('/api/account', function (req, res) {
-    console.log("")
-    console.log("-------- GET /account --------")
-    console.log("request headers", req.headers)
-    const response = accounts
-    console.log("response body", util.inspect(response, false, null, true /* enable colors */))
-    res.send(response)
-})
+app.get("/api/account/:primaryAccountId/card", function (req, res) {
+  const primaryAccountId = req.params.primaryAccountId;
+  console.log("");
+  console.log(`-------- GET /account/${primaryAccountId}/card --------`);
+  console.log("request headers", req.headers);
+  const response = primaryAccountId === "account-0001" ? cards1 : cards2;
+  console.log(
+    "response body",
+    util.inspect(response, false, null, true /* enable colors */)
+  );
+  res.send(response);
+});
 
-app.get('/api/account/:primaryAccountId/card', function (req, res) {
-    const primaryAccountId = req.params.primaryAccountId
-    console.log("")
-    console.log(`-------- GET /account/${primaryAccountId}/card --------`)
-    console.log("request headers", req.headers)
-    const response = primaryAccountId === 'account-0001' ? cards1 : cards2
-    console.log("response body", util.inspect(response, false, null, true /* enable colors */))
-    res.send(response)
-})
+app.post("/api/card/:cardId/replace/shipping", function (req, res) {
+  const cardId = req.params.cardId;
+  console.log(`-------- POST /card/${cardId}/replace/shipping --------`);
+  console.log("request headers", req.headers);
+  console.log(
+    "request body",
+    util.inspect(req.body, false, null, true /* enable colors */)
+  );
+  const response = cardReplaced;
+  console.log(
+    "response body",
+    util.inspect(response, false, null, true /* enable colors */)
+  );
+  res.send(response);
+});
 
-app.post('/api/card/:cardId/replace/shipping', function (req, res) {
-    const cardId = req.params.cardId
-    console.log(`-------- POST /card/${cardId}/replace/shipping --------`)
-    console.log("request headers", req.headers)
-    console.log("request body", util.inspect(req.body, false, null, true /* enable colors */))
-    const response = cardReplaced
-    console.log("response body", util.inspect(response, false, null, true /* enable colors */))
-    res.send(response)
-})
-
-
-
-
-// app.listen(4001)
-module.exports = app
+app.listen(4001);
+// module.exports = app
