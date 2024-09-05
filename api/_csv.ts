147eb6512ffc8daa3292a48a3853258639fbbfc7
Alex Hinds <alex.hinds141@gmail.com>
2024-09-05 10:44:46 +1000

    chore: loop through remote csv

--- api/_csv.ts
+++ api/_csv.ts
@@ -1,14 +1,14 @@
 import express from 'express'
 import { parse } from 'csv-parse'
-import type { ReadableStream as WebReadableStream } from "node:stream/web";
+import type { ReadableStream as WebReadableStream } from 'node:stream/web'
 import { Stream, Transform } from 'stream'
-    
+
 const router = express.Router()
 
 /**
  * @example
  * ```bash
- * curl -X POST -d '{ "url": "https://cdn.wsform.com/wp-content/uploads/2020/06/industry.csv" }' -H "Content-Type: application/json" http://localhost:4000/api/csv/ingest 
+ * curl -X POST -d '{ "url": "https://cdn.wsform.com/wp-content/uploads/2020/06/industry.csv" }' -H "Content-Type: application/json" http://localhost:4000/api/csv/ingest
  * ```
  */
 router.post('/ingest', async (req, res) => {
@@ -24,7 +24,9 @@ router.post('/ingest', async (req, res) => {
     return res.status(400).send('url is not valid')
   }
 
-  const readStream = await fetch(url).then(r => Stream.Readable.fromWeb(r.body as WebReadableStream));
+  const readStream = await fetch(url).then((r) =>
+    Stream.Readable.fromWeb(r.body as WebReadableStream),
+  )
 
   const parser = parse({
     delimiter: ',',
@@ -56,7 +58,7 @@ router.post('/ingest', async (req, res) => {
       }
       this.push(JSON.stringify(chunk))
       callback()
-    }
+    },
   })
 
   readStream
@@ -66,6 +68,53 @@ router.post('/ingest', async (req, res) => {
     .on('error', (err) => {
       res.status(400).send(err)
     })
+})
+
+// send expense to Remote API
+router.post('/send', async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send('body is required')
+  }
+
+  const { body: { records } } = req
+
+  if (!records || !records.length) {
+    return res.status(400).send('Records are required')
+  }
+
+  const errors = []
+
+  for (const record of records) {
+    try {
+      const response = await fetch('https://gateway.remote.com/v1/expenses', {
+        method: 'POST',
+        body: JSON.stringify({
+          amount: record.amount,
+          category: record.category,
+          currency: record.currency,
+          employment_id: record.employment_id,
+          expense_date: record.expense_date,
+          reviewed_at: record.reviewed_at,
+          reviewer_id: record.reviewer_id,
+          tax_amount: record.tax_amount,
+          timezone: record.timezone,
+          title: record.title,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: req.headers.authorization,
+        },
+      })
+      console.log('response', response.status)
+    } catch (e) {
+      errors.push(e)
+    }
+  }
+  if (errors.length) {
+    return res.status(400).send(errors)
+  }
+
+  res.status(200).send({ message: 'success' })
 })
 
 export default router
