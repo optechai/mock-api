ff4fe458b920242837414c946f7944fae7361f60
Xharmagne <xharmagne@gmail.com>
2024-09-11 11:03:24 +1000

    feat: mock some errors for expenses

--- api/_expenses.ts
+++ api/_expenses.ts
@@ -3,6 +3,12 @@ import express from 'express'
 const router = express.Router()
 
 router.post('/', async (req, res) => {
+  if (req.body.employment_id === 'Invalid') {
+    return res.status(422).send({ "errors": { "employment_id": ["Invalid ID"] } })
+  }
+  if (req.body.employment_id === 'NotReal') {
+    return res.status(404).send({ "message": "Employment not found" })
+  }
   const response = {
     originalRequestBody: req.body,
   }
