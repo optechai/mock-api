74171371079929bdd2cd652ef3730a92b9dcfbed
Alex Hinds <alex@optech.ai>
2024-12-10 09:55:53 +1100

    fix: add additional date information

--- api/_format-time.ts
+++ api/_format-time.ts
@@ -15,8 +15,25 @@ router.get('/', (request, res) => {
     })
     return
   }
+  const today = new Date()
   res.send({
-    formattedDate: new Date(date).toDateString()
+    formattedDate: new Date(date).toDateString(),
+    context: {
+      today: today.toDateString(),
+      tonight:  today.toDateString(),
+      yesterday: new Date(new Date().setDate(today.getDate() - 1)).toDateString(),
+      tomorrow: new Date(new Date().setDate(today.getDate() + 1)).toDateString(),
+      next_week: new Date(new Date().setDate(today.getDate() + 7)).toDateString(),
+    },
+    week: {
+      this_monday: new Date(new Date().setDate(today.getDate() + (1 - today.getDay() + 7) % 7)).toDateString(),
+      this_tuesday: new Date(new Date().setDate(today.getDate() + (2 - today.getDay() + 7) % 7)).toDateString(),
+      this_wednesday: new Date(new Date().setDate(today.getDate() + (3 - today.getDay() + 7) % 7)).toDateString(),
+      this_thursday: new Date(new Date().setDate(today.getDate() + (4 - today.getDay() + 7) % 7)).toDateString(),
+      this_friday: new Date(new Date().setDate(today.getDate() + (5 - today.getDay() + 7) % 7)).toDateString(),
+      this_saturday: new Date(new Date().setDate(today.getDate() + (6 - today.getDay() + 7) % 7)).toDateString(),
+      this_sunday: new Date(new Date().setDate(today.getDate() + (0 - today.getDay() + 7) % 7)).toDateString(),
+    }
   })
 })
 
